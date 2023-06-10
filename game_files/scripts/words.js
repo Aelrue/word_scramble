2231069f1835c42128b5fae002c7d8cc4e3c0054
Aelrue <littlehoatzin@gmail.com>
2023-06-10 16:50:35 -0500

    added words and hints to words.js. Created initGame variable in script.js

---
+++ game_files/scripts/words.js
@@ -0,0 +1,158 @@
+const words = [
+  {
+    word: "fry",
+    hint: "To cook in oil",
+  },
+  {
+    word: "mud",
+    hint: "A mix of soil and water",
+  },
+  {
+    word: "toe",
+    hint: "A digit on the foot",
+  },
+  {
+    word: "one",
+    hint: "The lowest cardinal number",
+  },
+  {
+    word: "key",
+    hint: "Used for locking and unlocking",
+  },
+  {
+    word: "copy",
+    hint: "To duplicate",
+  },
+  {
+    word: "frog",
+    hint: "Amphibious animal",
+  },
+  {
+    word: "take",
+    hint: "To remove something",
+  },
+  {
+    word: "word",
+    hint: "Single element of spoken or written language",
+  },
+  {
+    word: "game",
+    hint: "Played for enjoyment",
+  },
+  {
+    word: "cold",
+    hint: "Low in temperature",
+  },
+  {
+    word: "exit",
+    hint: "To leave",
+  },
+  {
+    word: "jump",
+    hint: "To come off the ground",
+  },
+  {
+    word: "aged",
+    hint: "Having lived or existed for a long time",
+  },
+  {
+    word: "acid",
+    hint: "Typically corrosive or sour liquid",
+  },
+  {
+    word: "width",
+    hint: "measurement from side to side",
+  },
+  {
+    word: "addition",
+    hint: "The process of adding numbers",
+  },
+  {
+    word: "meeting",
+    hint: "Event in which people come together",
+  },
+  {
+    word: "number",
+    hint: "Math symbol used for counting",
+  },
+  {
+    word: "exchange",
+    hint: "The act of trading",
+  },
+  {
+    word: "canvas",
+    hint: "Piece of fabric for oil painting",
+  },
+  {
+    word: "garden",
+    hint: "Space for planting flower and plant",
+  },
+  {
+    word: "position",
+    hint: "Location of someone or something",
+  },
+  {
+    word: "feather",
+    hint: "Hair like outer covering of bird",
+  },
+  {
+    word: "comfort",
+    hint: "A pleasant feeling of relaxation",
+  },
+  {
+    word: "tongue",
+    hint: "The muscular organ of mouth",
+  },
+  {
+    word: "expansion",
+    hint: "The process of increase or grow",
+  },
+  {
+    word: "country",
+    hint: "A politically identified region",
+  },
+  {
+    word: "group",
+    hint: "A number of objects or persons",
+  },
+  {
+    word: "taste",
+    hint: "Ability of tongue to detect flavour",
+  },
+  {
+    word: "store",
+    hint: "Large shop where goods are traded",
+  },
+  {
+    word: "field",
+    hint: "Area of land for farming activities",
+  },
+  {
+    word: "friend",
+    hint: "Person other than a family member",
+  },
+  {
+    word: "pocket",
+    hint: "A bag for carrying small items",
+  },
+  {
+    word: "needle",
+    hint: "A thin and sharp metal pin",
+  },
+  {
+    word: "expert",
+    hint: "Person with extensive knowledge",
+  },
+  {
+    word: "statement",
+    hint: "A declaration of something",
+  },
+  {
+    word: "second",
+    hint: "One-sixtieth of a minute",
+  },
+  {
+    word: "library",
+    hint: "Place containing collection of books",
+  },
+];
