becfc55a700085bebae3a5e94f5a854283044cd4
Aelrue <littlehoatzin@gmail.com>
2023-06-10 23:24:53 -0500

    swapped places of words.js and script.js script source so words.js comes first. Console errored before as words in script.js was undefined. .random, .split, and .join randomize words, jumble letters, and join back together on the DOM without commas

--- game_files/scripts/script.js
+++ game_files/scripts/script.js
@@ -1,6 +1,17 @@
+const wordText = document.querySelector(".word");
+hintText = document.querySelector(".hint span");
+
 const initGame = () => {
   let randomObj = words[Math.floor(Math.random() * words.length)];
-  console.log(randomObj);
+  let wordArray = randomObj.word.split(""); //splitting each letter of random word
+  for (let i = wordArray.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1)); //getting random number
+    [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]]; //swapping letters randomly
+  }
+  // remember, if this were simply: wordText.innerText = wordArray, the letters would be separated by commas.
+  // in this case, .join("") is used to join the letters together with no spaces or commas
+  wordText.innerText = wordArray.join(""); //passing shuffled word as wordText
+  hintText.innerText = randomObj.hint; //passing random object hint as hintText
+  console.log(randomObj.word);
 };
-
 initGame();
